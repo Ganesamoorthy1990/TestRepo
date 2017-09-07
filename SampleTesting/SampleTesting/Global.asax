cd3a8a0f3a92ca6f3f1f8a1101badb8ed855c2d6
Sunera <sunerahanif@gmail.com>
2017-09-07 12:09:46 +0530

    Started our new project

---
+++ SampleTesting/SampleTesting/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SampleTesting.MvcApplication" Language="C#" %>
---
+++ SampleTesting/SampleTesting/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SampleTesting.Startup))]
+namespace SampleTesting
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
